9413d8ac97495b4194a108018a43b9ab224c4f74
Steve Godin <steve.god@gmail.com>
2017-03-14 13:50:26 -0700

    Add state demo for HelloWorld

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -11,6 +11,7 @@ const HelloWorld = function (props) {
 
 // When creating a component module, do not forget to export your
 // react component as default
+/*
 export default props => {
   // you can declare inline styles for components with a plain js object
   // CSS property names will the objects properties and should named
@@ -30,3 +31,89 @@ export default props => {
     Hello World and Hello {props.name}
   </p>;
 }
+*/
+
+// in class-based React components, props is a property of this
+// this style of component must extend from React.Component
+export default class HelloWorld extends React.Component {
+  constructor (props) {
+    // Always call super with props
+    // super will call the same name method of the parent class
+    // in this, super is calling the constructor defined in React.Component
+    super(props);
+    // Always call super before doing any changes to this
+
+    //👇 set initial state
+    this.state = {
+      hovered: false
+    };
+
+    // bind event handlers and callbacks in the constructor
+    this.handleLeave = this.handleLeave.bind(this);
+  }
+  handleLeave (event) {
+    // as in regular event listeners with .addEventListener,
+    // React event callbacks will also receive an event object
+    this.setState({hovered: false});
+  }
+  // this style of component requires at the very minimum
+  // a render method which returns JSX (or, a React element) just
+  // like function-based components
+  render() {
+    const styles = {
+      backgroundColor: this.props.bgColor || 'Tomato',
+      color: 'Aquamarine',
+      fontFamily: 'Helvetica',
+      fontWeight: 100,
+      fontSize: '2em',
+      padding: '0.5rem'
+    };
+
+    const greeting = this.state.hovered ? 'Bye' : 'Hello';
+
+    return (
+      <p
+        onMouseEnter={() => {
+          console.log('Mouse Entered');
+          // Only do state changes with the method this.setState
+          // .setState takes an object as its first argument that
+          // will be merged with the state object in this.state
+          this.setState({hovered: true}, function () {
+            // .setState can also take a callback as a second argument
+            // it will be called once the state change is complete
+            console.log('State change complete! 🛠')
+          });
+          // this.setState is an asynchronous function
+          // do not write code immediately after it that depends on the state
+        }}
+        onMouseLeave={this.handleLeave/*.bind(this) avoid binding inline because it creates
+          a function everytime render is called (and, it's called often)*/}
+        style={styles}
+        className='styling pimpin'>
+        {greeting} World and {greeting} {this.props.name}
+      </p>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* */
