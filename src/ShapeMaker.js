2dce81a049e60fdc181371a48a0f4c1d8a876e32
Steve Godin <steve.god@gmail.com>
2017-03-15 12:04:43 -0700

    Add solution to lab Shape Destroyer

--- src/ShapeMaker.js
+++ src/ShapeMaker.js
@@ -1,7 +1,17 @@
 import React, {Component} from 'react';
 
-function Shape ({color = 'Blue', type = 'Square'}) {
+function Shape ({id, color = 'Blue', type = 'Square', onClick = () => {}}) {
+  const handleClick = event => {
+    // when the shape div gets clicked, call onClick callback
+    // with information about the shape
+    onClick({id, color, type})
+    // this is alternative to just forwarding the event listener callback
+    // instead we choose when & what happens with the callback passed to
+    // Shape's onClick prop
+  }
+
   return <div
+    onClick={handleClick}
     style={{
       backgroundColor: color,
         height: '3rem',
@@ -11,9 +21,9 @@ function Shape ({color = 'Blue', type = 'Square'}) {
     className='Shape'></div>;
 }
 
-function ShapeHolder ({shapes = []}) {
+function ShapeHolder ({shapes = [], onShapeClick = () => {}}) {
   return <div className="ShapeHolder">
-    { shapes.map((shape, i) => <Shape key={i} {...shape} />) }
+    { shapes.map(shape => <Shape key={shape.id} {...shape} onClick={onShapeClick} />) }
   </div>
 }
 
@@ -25,6 +35,7 @@ export default class ShapeMaker extends Component {
     }
 
     this.addShape = this.addShape.bind(this);
+    this.removeShape = this.removeShape.bind(this);
   }
 
   addShape (event) {
@@ -33,9 +44,22 @@ export default class ShapeMaker extends Component {
 
     this.setState({
       shapes: shapes.concat({
+        // I'm adding an id to Shapes to make easy 
+        id: shapes.length,
         type: this.refs.type.value,
         color: this.refs.color.value
       })
+    })
+  }
+
+  removeShape ({id}) {
+    const {shapes} = this.state;
+    // removing a shape doesn't involve the DOM, because the shapes shown
+    // on our page are based the array of objects we have in our state
+    // instead we just the shape-like object from our shapes in array in our
+    // state. On the rerender, react will no rerender the removed shape
+    this.setState({
+      shapes: shapes.filter(shape => shape.id !== id)
     })
   }
 
@@ -58,7 +82,7 @@ export default class ShapeMaker extends Component {
       </form>
 
       <h1>Shape Holder</h1>
-      <ShapeHolder shapes={this.state.shapes} />
+      <ShapeHolder onShapeClick={this.removeShape} shapes={this.state.shapes} />
     </div>
   }
 }
